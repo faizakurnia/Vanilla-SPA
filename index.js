3c5a7d2e211f7c6847218e4f9f157b8c38040f34
faiza10 <faizakurniagck@gmail.com>
2023-02-17 08:49:55 +0700

    04_full_client_side_rendering

--- index.js
+++ index.js
@@ -1,51 +1,83 @@
-const linkHome = document.getElementById("link-home");
-const linkAbout = document.getElementById("link-about");
 const root = document.getElementById("root");
 
-linkHome.onclick = function (event) {
-  event.preventDefault();
-  history.pushState(null, "", event.target.href);
-  root.innerHTML = "";
-  root.appendChild(HomePage());
-};
+function Navbar() {
+  const linkHome = document.createElement("a");
+  linkHome.href = "#home";
+  linkHome.textContent = "Home";
+  linkHome.onclick = function (event) {
+    event.preventDefault();
+    history.pushState(null, "", event.target.href);
+    root.innerHTML = "";
+    root.appendChild(HomePage());
+  };
 
-linkAbout.onclick = function (event) {
-  event.preventDefault();
-  history.pushState(null, "", event.target.href);
-  root.innerHTML = "";
-  root.appendChild(AboutPage());
-};
+  const linkAbout = document.createElement("a");
+  linkAbout.href = "#about";
+  linkAbout.textContent = "About";
+  linkAbout.onclick = function (event) {
+    event.preventDefault();
+    history.pushState(null, "", event.target.href);
+    root.innerHTML = "";
+    root.appendChild(AboutPage());
+  };
+
+  const div = document.createElement("div");
+  div.append(linkHome);
+  div.append(linkAbout);
+
+  return div;
+}
 
 function HomePage() {
-    const p = document.createElement("p");
-    p.textContent = "Welcome to Home Page";
-  
-    const textPreview = document.createElement("p");
-  
-    const input = document.createElement("input");
-    input.placeholder = "enter your name";
-    input.oninput = function (event) {
-      textPreview.textContent = event.target.value;
-    };
-  
-    const div = document.createElement("div");
-    div.append(p);
-    div.append(input);
-    div.append(textPreview);
-  
-    return div;
-  }
+  const navbar = Navbar();
 
-  function AboutPage() {
-    const p = document.createElement("p");
-    p.textContent = "Welcome to About Page";
-    return p;
-  }
+  const p = document.createElement("p");
+  p.textContent = "Welcome to Home Page";
 
-if (window.location.hash == "#home") {
-  root.textContent = "Welcome to Home Page";
-} else if (window.location.hash == "#about") {
-  root.textContent = "Welcome to About Page";
+  const textPreview = document.createElement("p");
+
+  const input = document.createElement("input");
+  input.placeholder = "enter your name";
+  input.oninput = function (event) {
+    textPreview.textContent = event.target.value;
+  };
+
+  const div = document.createElement("div");
+  div.append(navbar);
+  div.append(p);
+  div.append(input);
+  div.append(textPreview);
+
+  return div;
+}
+
+function AboutPage() {
+  const linkHome = document.createElement("a");
+  linkHome.href = "#home";
+  linkHome.textContent = "Back to Home";
+  linkHome.onclick = function (event) {
+    event.preventDefault();
+    history.pushState(null, "", event.target.href);
+    root.innerHTML = "";
+    root.appendChild(HomePage());
+  };
+
+  const p = document.createElement("p");
+  p.textContent = "Welcome to About Page";
+
+  const div = document.createElement("div");
+  div.appendChild(linkHome);
+  div.appendChild(p);
+  return div;
+}
+
+if (window.location.pathname == "#home") {
+  root.innerHTML = "";
+  root.appendChild(HomePage());
+} else if (window.location.pathname == "#about") {
+  root.innerHTML = "";
+  root.appendChild(AboutPage());
 } else {
-  root.textContent = "Welcome to Home Page";
+  root.innerHTML = "";
+  root.appendChild(HomePage());
 }
